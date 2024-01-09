a56ce1c971f426e160cea7a7f172aa544fd8dc6d
Huynguyen <huynguyen7039@gmail.com>
2024-01-09 19:57:17 +0700

    bai2

---
+++ Chuong 3/Bai2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define pi 3.14159
+
+int main()
+{
+    float r, CV, S;
+    printf("Chương trình tính chu vi và diện tích hình tròn \n");
+    printf("Nhập bán kính r=");
+    scanf("%f", &r);
+    CV = 2 * pi * r;
+    S = pi * r * r;
+    printf("\n Chu vi = %.2f \n Diện tích = %.2f", CV, S);
+    return 0;
+}
