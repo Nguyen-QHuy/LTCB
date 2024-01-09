6d780c5253500829fcfa6341e423ea7de46a8ecc
Huynguyen <huynguyen7039@gmail.com>
2024-01-09 19:23:29 +0700

    init

---
+++ Chuong 3/3.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    printf("************************************************\n");
+    printf("\t\t\tTHIEP MOI\n");
+    printf("\tThân mời bạn : Nguyễn Mạnh Hùng \n");    
+    printf("\tTới dự lễ sinh nhật của mình \n");
+    printf("\tVào lúc 19h ngày 12/10/2005\n");    
+    printf("\tTại 05/42 Trần Phú - Cần Thơ\n");
+    printf("\tRất mong được đón tiếp !\n");
+    printf("\t\t\t\t\tHồ Thu Hương\n");
+    printf("************************************************\n");
+    return 0;
+}
