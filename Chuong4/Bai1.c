b4d43c187bd792e608deae2cafe23aaf367032e1
Huynguyen <huynguyen7039@gmail.com>
2024-01-15 23:25:49 +0700

    upload

---
+++ Chuong4/Bai1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, num3, max;
+    printf("Chương trình tìm số lớn nhất\n");
+    printf("Nhập số thứ nhất: ");
+    scanf("%d", &num1);
+    printf("Nhập số thứ hai: ");
+    scanf("%d", &num2);
+    printf("Nhập số thứ ba: ");
+    scanf("%d", &num3);
+
+    if (num1 > num2)
+    {
+        max = num1;
+    }
+    else if (num2 > num3)
+    {
+        max = num2;
+    }
+    else
+    {
+        max = num3;
+    }
+
+    printf("Số lớn nhất trong ba số là: %d ", max);
+    return 0;
+}
---
+++ Chuong4/Bai6.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int month, year;
+    printf("Nhập tháng: ");
+    scanf("%2d", &month);
+    printf("Nhập năm: ");
+    scanf("%4d", &year);
+
+    switch (month)
+    {
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        printf("Số ngày trong tháng là 30 ngày");
+        break;
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        printf("Số ngày trong tháng là 31 ngày");
+        break;
+    case 2:
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        {
+            printf("Số ngày trong tháng là 29 ngày");
+        }
+        else
+        {
+            printf("Số ngày trong tháng là 28 ngày");
+        }
+        break;
+    default:
+        printf("Không có tháng này");
+        break;
+    }
+    return 0;
+}
